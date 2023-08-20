07e8c8c8375f87b8de6431d92c3e480ad8a9b016
YuYuanNg <you@example.com>
2023-08-20 17:22:57 +0800

    debug added

--- index.test.js
+++ index.test.js
@@ -19,28 +19,3 @@ describe('Lambda Handler', () => {
   });
 });
 
-import {
-    SecretsManagerClient,
-    GetSecretValueCommand,
-  } from "@aws-sdk/client-secrets-manager";
-  
-  const secret_name = "yuyuan-secret";
-  
-  const client = new SecretsManagerClient({
-    region: "ap-southeast-1",
-  });
-  
-  let response;
-  
-  try {
-    response = await client.send(
-      new GetSecretValueCommand({
-        SecretId: secret_name,
-        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-      })
-    );
-  } catch (error) {
-    // For a list of exceptions thrown, see
-    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    throw error;
-  }
